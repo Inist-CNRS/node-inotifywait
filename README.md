c5fb85d03c1800317bbe8781d30af819f5f0b2af
Stéphane Gully <stephane.gully@gmail.com>
2013-07-06 21:30:12 +0200

    first import

---
+++ README.md
@@ -0,0 +1,43 @@
+# node-inotifywait
+
+Yet another nodejs fs.watch implementation that can watch:
+
+* folders recursively
+* big number of directories and files
+* with low CPU use
+
+This implementation is a wrapper above the inotifywait system command.
+
+### Why
+
+Because other implementations:
+* [fs.watch](http://nodejs.org/api/fs.html) 
+* [node-watch](https://github.com/yuanchuan/node-watch)
+* [chokidar](https://github.com/paulmillr/chokidar)
+* [watch](https://github.com/mikeal/watch)
+
+Are not performant for huge number of directories and files watching. Some are not recursive, other have high CPU usage when watching lot of directories and files. 
+
+### Installation
+
+```bash
+npm install inotifywait
+```
+
+### Example
+
+```js
+var inw = require('inotifywait');
+
+var watch1 = inw.watch('/tmp/', { recursive: false });
+watch1.on('add', function (filename) {
+  console.log(filename + ' added');
+  watch1.close(); // stop watching
+});
+
+var watch2 = inw.watch('/var/log/', { recursive: true });
+watch2.on('change', function (filename) {
+  console.log(filename + ' changed');
+  watch2.close(); // stop watching
+});
+``` 
---
+++ test/inotifywait.js
@@ -0,0 +1,69 @@
+var expect = require('chai').expect;
+var iws    = require('../index.js');
+var uuid   = require('uuid');
+var fs     = require('fs');
+var mkdirp = require('mkdirp');
+var remove = require('remove');
+
+var fakeFile = '';
+before(function(){
+  fakeFile = generateFakeFile('fake1');
+});
+
+describe('inotifywait', function () {
+  describe('.watch()', function () {
+    it('should detect when a new file is added', function (done) {
+      var f = '';
+      setTimeout(function () {
+        f = generateFakeFile('fake2');
+      }, 10);
+      var w = iws.watch(__dirname + '/data');
+      w.on('add', function (filename) {
+        expect(filename).to.eql(f);
+        w.close();
+        done();
+      });
+    });
+
+    it('should detect when a file is modified', function (done) {
+      setTimeout(function () {
+        fs.writeFileSync(fakeFile, '...');
+      }, 10);
+      var w = iws.watch(__dirname + '/data');
+      w.on('change', function (filename) {
+        expect(filename).to.eql(fakeFile);
+        w.close();
+        done();
+      });
+    })
+
+    it('should detect when a file is removed', function (done) {
+      setTimeout(function () {
+        remove.removeSync(fakeFile);
+      }, 10);
+      var w = iws.watch(__dirname + '/data');
+      w.on('unlink', function (filename) {
+        expect(filename).to.eql(fakeFile);
+        w.close();
+        done();
+      });
+    })
+
+  })
+});
+
+after(function(){
+  remove.removeSync(__dirname + '/data');
+});
+
+function generateFakeFile(name) {
+  //var id = uuid.v4();
+  var path = __dirname + '/data'; // + id[0] + '/' + id[1] + '/' + id[2];
+  var file = path + '/' + name;
+
+  mkdirp.sync(path);
+  //console.log(path + ' created [' + i + ']');
+  fs.writeFileSync(file, '.');
+  //console.log(file + ' created [' + i + ']');
+  return file;
+}
